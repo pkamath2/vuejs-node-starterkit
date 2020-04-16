5f574f643b4cc37e258e3795b24a0da71b2185bd
pkamath2 <git.pkamath2@gmail.com>
2020-04-16 10:29:22 +0800

    Updates

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!! Yeppiee!!"/>
+    <img alt="Vue logo" src="./assets/techladies-logo.png">
+    <test-component msg="#bootcamp6"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
-</script>
+</script> 
 
 <style>
 #app {
--- src/client/main.js
+++ src/client/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
 Vue.config.productionTip = false
+
+/* Auto importing all components from components directory */
+const all_components = require.context('./components/', true, /(\.html|\.js|\.vue)$/)
+all_components.keys().forEach(fileName => {
+  const componentConfig = all_components(fileName)
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 new Vue({
   render: h => h(App),
